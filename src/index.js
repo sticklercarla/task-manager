5b2e67a0929b61a443920c1ca4abb6f093cb7a1c
sticklercarla <carlastickler@gmail.com>
2020-04-26 09:57:52 -0400

    validations and update properties

--- src/index.js
+++ src/index.js
@@ -14,3 +14,22 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port' + port)
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = 'Banana123'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('Banana123', hashedPassword)
+    console.log(isMatch)
+}
+
+
+myFunction()
+
+// carla -> fjdkjaljfnjaglajgk -> andrew
+// mypass -> fjslakjflksdjfksdajflkdsaj (NOT reversible)
