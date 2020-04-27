278e0fda22d23488b9f703970df1c845da7b7d68
sticklercarla <carlastickler@gmail.com>
2020-04-27 09:59:46 -0400

    more auth and jwt

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+//How to use Middleware
+// app.use((req, res, next) => {
+//     if (req.method == 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+//maintence call
+// app.use((req, res, next) => {
+//     res.status(503).send('Site unavailable for site maintenance!')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -15,18 +29,27 @@ app.listen(port, () => {
     console.log('Server is up on port' + port)
 })
 
-const bcrypt = require('bcryptjs')
+// const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const myFunction = async () => {
-    const password = 'Banana123'
-    const hashedPassword = await bcrypt.hash(password, 8)
+    const token = jwt.sign({ _id: 'abcdsdf' }, 'thisismynewsecret', { expiresIn: '7 days' })
+    console.log(token)
 
-    console.log(password)
-    console.log(hashedPassword)
-
-    const isMatch = await bcrypt.compare('Banana123', hashedPassword)
-    console.log(isMatch)
+    const data = jwt.verify(token, 'thisismynewsecret')
+    console.log(data)
 }
+
+// const myFunction = async () => {
+//     const password = 'Banana123'
+//     const hashedPassword = await bcrypt.hash(password, 8)
+
+//     // console.log(password)
+//     // console.log(hashedPassword)
+
+//     const isMatch = await bcrypt.compare('Banana123', hashedPassword)
+//     // console.log(isMatch)
+// }
 
 
 myFunction()
